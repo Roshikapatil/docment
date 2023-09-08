a33c9489f51fb7857fd68ef2162924e3c7636f47
Roshika patil <roshikapatil9@gmail.com>
2023-09-08 15:44:47 +0530

    first commit

---
+++ vanshika java/HelloJava.java
@@ -0,0 +1,16 @@
+class HelloJava
+{
+public static void main(String args[])
+{
+    int a=4;
+System.out.println("check even or odd ");
+if(a%2==0)
+{
+   System.out.println("A is a even nmber  "); 
+}
+else
+{
+    System.out.println("A is a odd nmber  ");
+}
+}
+}
